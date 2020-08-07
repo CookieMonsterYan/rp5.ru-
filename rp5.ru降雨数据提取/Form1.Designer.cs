086629f1eae081c115abd89237927f02ca011f81
cookie <cookie.monster.yan@hotmail.com>
2020-08-07 16:44:54 +0800

    1st

--- rp5.ru降雨数据提取/Form1.Designer.cs
+++ rp5.ru降雨数据提取/Form1.Designer.cs
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgv_csvFiles = new System.Windows.Forms.DataGridView();
+            this.CsvFile = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_csvFiles = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtb_result = new System.Windows.Forms.TextBox();
+            this.btn_result = new System.Windows.Forms.Button();
+            this.pb = new System.Windows.Forms.ProgressBar();
+            this.btn_run = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_csvFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "降雨csv文件";
+            // 
+            // dgv_csvFiles
+            // 
+            this.dgv_csvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_csvFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CsvFile});
+            this.dgv_csvFiles.Location = new System.Drawing.Point(29, 80);
+            this.dgv_csvFiles.Name = "dgv_csvFiles";
+            this.dgv_csvFiles.RowTemplate.Height = 23;
+            this.dgv_csvFiles.Size = new System.Drawing.Size(545, 197);
+            this.dgv_csvFiles.TabIndex = 2;
+            // 
+            // CsvFile
+            // 
+            this.CsvFile.HeaderText = "csv文件";
+            this.CsvFile.Name = "CsvFile";
+            this.CsvFile.Width = 500;
+            // 
+            // btn_csvFiles
+            // 
+            this.btn_csvFiles.Location = new System.Drawing.Point(111, 38);
+            this.btn_csvFiles.Name = "btn_csvFiles";
+            this.btn_csvFiles.Size = new System.Drawing.Size(35, 23);
+            this.btn_csvFiles.TabIndex = 3;
+            this.btn_csvFiles.Text = "...";
+            this.btn_csvFiles.UseVisualStyleBackColor = true;
+            this.btn_csvFiles.Click += new System.EventHandler(this.btn_csvFiles_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "保存降雨文件";
+            // 
+            // txtb_result
+            // 
+            this.txtb_result.Location = new System.Drawing.Point(111, 297);
+            this.txtb_result.Name = "txtb_result";
+            this.txtb_result.Size = new System.Drawing.Size(390, 21);
+            this.txtb_result.TabIndex = 5;
+            // 
+            // btn_result
+            // 
+            this.btn_result.Location = new System.Drawing.Point(508, 297);
+            this.btn_result.Name = "btn_result";
+            this.btn_result.Size = new System.Drawing.Size(35, 23);
+            this.btn_result.TabIndex = 6;
+            this.btn_result.Text = "...";
+            this.btn_result.UseVisualStyleBackColor = true;
+            this.btn_result.Click += new System.EventHandler(this.btn_result_Click);
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(29, 372);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(472, 23);
+            this.pb.TabIndex = 7;
+            // 
+            // btn_run
+            // 
+            this.btn_run.Location = new System.Drawing.Point(508, 371);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(75, 23);
+            this.btn_run.TabIndex = 8;
+            this.btn_run.Text = "RUN";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(602, 450);
+            this.Controls.Add(this.btn_run);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.btn_result);
+            this.Controls.Add(this.txtb_result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_csvFiles);
+            this.Controls.Add(this.dgv_csvFiles);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_csvFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv_csvFiles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CsvFile;
+        private System.Windows.Forms.Button btn_csvFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtb_result;
+        private System.Windows.Forms.Button btn_result;
+        private System.Windows.Forms.ProgressBar pb;
+        private System.Windows.Forms.Button btn_run;
     }
 }
 
--- rp5.ru降雨数据提取/Form1.cs
+++ rp5.ru降雨数据提取/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,148 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace rp5.ru降雨数据提取
 {
     public partial class Form1 : Form
     {
+        private List<string> datetimes = new List<string>();
+        private List<float> rains = new List<float>();
+        private Regex split = new Regex("\".*?\"");  // *? 表示匹配上一个元素0次或多次，并且次数尽可能少
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_csvFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+
+            openFileDlg.Filter = "All files (*.*)|*.*| CSV files(*.csv) | *.csv";
+            openFileDlg.FilterIndex = 2;
+            openFileDlg.RestoreDirectory = true;
+            openFileDlg.Multiselect = true;
+
+            if (openFileDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach(string file in openFileDlg.FileNames)
+                {
+                    dgv_csvFiles.Rows.Add(file);
+                }
+            }
+        }
+
+        private void btn_result_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "All files (*.*)|*.*| CSV files(*.csv) | *.csv";
+            sfd.FilterIndex = 2;
+            sfd.RestoreDirectory = true;
+
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtb_result.Text = sfd.FileName;
+            }
+        }
+
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            pb.Value = 0;
+
+            string result = txtb_result.Text;
+            List<string> files = new List<string>();
+            int num = 0;
+            foreach(DataGridViewRow row in dgv_csvFiles.Rows)
+            {
+                string file = (string)row.Cells[0].Value;
+                if (File.Exists(file))
+                {
+                    files.Add(file);
+                    num++;
+                }
+            }
+
+            foreach(string file in files)
+            {
+                ClipRRR(file);
+                pb.Value += 100 / (num + 1);
+            }
+
+            SaveRRR(result);
+            pb.Value = 100;
+        }
+
+        private void ClipRRR(string file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                string line;
+                bool headLine = false;
+                while((line = sr.ReadLine()) != null)
+                {
+                    line = line.Trim();
+                    if(line.Length == 0 || line[0].Equals('#'))
+                    {
+                        continue;
+                    }
+
+                    MatchCollection match = split.Matches(line);
+                    if(match.Count < 29)
+                    {
+                        continue;
+                    }
+
+                    if(headLine)
+                    {
+                        string time = match[0].Value.Replace('"', ' ').Trim();
+                        if(datetimes.Contains(time))
+                        {
+                            continue;
+                        }
+                        datetimes.Add(time);
+                        string r = match[23].Value.Replace('"', ' ').Trim();
+                        if (r.Length <= 0)
+                        {
+                            rains.Add(0.0f);
+                        }
+                        else
+                        {
+                            float rain = 0.01f;
+                            try
+                            {
+                                rain = float.Parse(r);
+                                rains.Add(rain);
+                            }
+                            catch(Exception ex)
+                            {
+                                rains.Add(0.01f);
+                            }
+                            
+                        }
+                    }
+                    else
+                    {
+                        headLine = true;
+                    }
+                }
+            }
+        }
+
+        private void SaveRRR(string file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Create))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.WriteLine("Time,RRR");
+                for(int i = 0; i < datetimes.Count; i++)
+                {
+                    sw.WriteLine(datetimes[i] + "," + rains[i].ToString());
+                }
+            }
         }
     }
 }
